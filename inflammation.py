38fd602717ffdf8fc3b40ece8980c8c3e1e12f4a
Vlad Dracula <vlad@dracula.com>
2015-06-24 14:18:45 -0400

    Undo additions to program output and documentation
    We want to minimize unnecessary program output. Documentation
    was not accurate.

--- inflammation.py
+++ inflammation.py
@@ -1,11 +1,8 @@
 """
 Slice and dice data from an inflammation study.
-
-This program reads data from data/*.csv files.
 """
 
 
 import sys
 
-print 'There are', len(sys.argv), 'arguments'
 print sys.argv
